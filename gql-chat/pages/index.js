57c54b167c02aab05cb2757de6a7dc46e5cddf11
Ike <ikescode@gmail.com>
2021-05-15 19:44:43 -0500

    using apollo hooks

--- gql-chat/pages/index.js
+++ gql-chat/pages/index.js
@@ -1,66 +1,60 @@
 import Head from 'next/head';
-import { gql } from "@apollo/client";
-import client from "../lib/apollo-client";
+import { gql, useQuery, useMutation } from '@apollo/client';
 import styles from '../styles/Home.module.css';
 
-export async function getStaticProps() {
-  const { data } = await client.query({
-    query: gql`
-      query Channels {
-        channels {
-          name
-        }
-      }
-    `,
-  });
+const GET_CHANNELS = gql`
+  query Channels {
+    channels {
+      name
+    }
+  }
+`;
 
-  return {
-    props: {
-      channels: data.channels
-    },
- };
-}
+const ADD_CHANNEL = gql`
+  mutation addChannel($name: String!) {
+    channel(name: $name) {
+      id
+      name
+    }
+  }
+`;
 
-export default function Home({ channels }) {
+export default function Home() {
+
+  const { channels, loading, error } = useQuery(GET_CHANNELS);
+  const [addChannel, { data }] = useMutation(ADD_CHANNEL);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GQL Chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">GQL Chat</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by choosing a channel
         </p>
 
         <div className={styles.grid}>
           {
-            channels.map(({ name }, i) => (
-              <a key={i} href="#" className={styles.card}>
+            channels.map(({ id, name }) => (
+              <a key={id} href="#" className={styles.card}>
                 <h2>{name}</h2>
                 {/* <p>...</p> */}
               </a>
             ))
             }
         </div>
+        <button onClick={()=>addChannel({ name: 'test' })} className={styles.button}>New Channel</button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
